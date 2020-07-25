6335a8c50e21770d338a989336652c7d1d41e3fe
Alexander Avdeev <alexander.lex.avdeev@gmail.com>
2020-07-25 15:47:28 +0300

    feat(RestApi): flashsales rest api added

---
+++ lib/rest_api.dart
@@ -0,0 +1,14 @@
+import 'package:http/http.dart';
+
+import 'network_client.dart';
+
+class RestApi {
+  static const _host = "flashsales.burshtat.by";
+
+  final _networkClient = NetworkClient(_host);
+
+  Future<String> search(String query) async {
+    Response _response = await _networkClient.get("/api/v1/search/$query");
+    return _response.body;
+  }
+}
