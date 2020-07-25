b12a5ecf3e10837c9a94406995f45d621b51a683
Alexander Avdeev <alexander.lex.avdeev@gmail.com>
2020-07-25 18:43:39 +0300

    feat(Repository): repository class added

---
+++ lib/repository.dart
@@ -0,0 +1,16 @@
+import 'dart:convert';
+
+import 'product.dart';
+import 'rest_api.dart';
+
+class Repository {
+  final _restApi = RestApi();
+
+  Future<Product> getProduct(String query) async {
+    return Product.fromJson(
+      json.decode(
+        await _restApi.search(query),
+      ),
+    );
+  }
+}
