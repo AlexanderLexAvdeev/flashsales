d91cf8613c7dece10f3e9323a8d480137bea7420
Alexander Avdeev <alexander.lex.avdeev@gmail.com>
2020-07-27 22:23:53 +0300

    feat(ProductListView): added list view for product

---
+++ lib/widgets/product_list_view.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+import '../product.dart';
+import 'list_item.dart';
+
+class ProductListView extends StatelessWidget {
+  final Product _product;
+
+  const ProductListView(this._product);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scrollbar(
+      child: ListView.separated(
+        padding: EdgeInsets.only(
+          top: 8,
+          bottom: 8,
+        ),
+        physics: BouncingScrollPhysics(),
+        itemCount: _product.list.length,
+        itemBuilder: (BuildContext context, int index) {
+          return ListItem(_product.list[index]);
+        },
+        separatorBuilder: (BuildContext context, int index) {
+          return Divider();
+        },
+      ),
+    );
+  }
+}
