d1130fadecb2ed4a715f1934687300f322f38961
Alexander Avdeev <alexander.lex.avdeev@gmail.com>
2020-07-27 22:14:15 +0300

    feat(ListItem): added list item for product unit

---
+++ lib/widgets/list_item.dart
@@ -0,0 +1,42 @@
+import 'package:flashsales/product.dart';
+import 'package:flutter/material.dart';
+
+class ListItem extends StatelessWidget {
+  final ProductUnit productUnit;
+
+  const ListItem(this.productUnit);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Image.network(
+        productUnit.image,
+        width: 48,
+        height: 48,
+        loadingBuilder: (BuildContext context, Widget child,
+            ImageChunkEvent loadingProgress) {
+          if (loadingProgress == null) {
+            return child;
+          }
+          return Center(
+            child: CircularProgressIndicator(
+              value: loadingProgress.expectedTotalBytes != null
+                  ? loadingProgress.cumulativeBytesLoaded /
+                      loadingProgress.expectedTotalBytes
+                  : null,
+            ),
+          );
+        },
+      ),
+      title: Text(
+        productUnit.name,
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      trailing: Text(
+        "${productUnit.price} BYN",
+      ),
+    );
+  }
+}
